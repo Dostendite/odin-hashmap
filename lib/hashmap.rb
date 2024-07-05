1673ab0a4becc71185aff63009ff7be5e15b5220
Dostendite <dostendite@outlook.es>
2024-07-05 12:50:08 +0200

    Add update_capacity method
    The hashmap capacity and load factor are set to
    16 and 0.8 by default, once the current
    capacity goes above their product, it increments
    the bucket array by 8.

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,5 +1,5 @@
 class HashMap
-  def initialize(capacity=nil, load_factor=nil)
+  def initialize(capacity=16, load_factor=0.8)
     @capacity = capacity
     @load_factor = load_factor
     @REAL_NUMBER = 0.5*(Math.sqrt(5)-1)
@@ -15,5 +15,13 @@ class HashMap
     key.each_char { |char| prime_number * hash_code + char.ord }
     
     hash_code
+  end
+
+  def update_capacity
+    capacity_limit = (@capacity * @load_factor).floor(1)
+
+    if @capacity > capacity_limit
+      capacity += 8
+    end
   end
 end
