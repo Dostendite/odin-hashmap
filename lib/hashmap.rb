251d9ab7acd7e48c2e1a7386be16d4c6e0fe5422
Dostendite <dostendite@outlook.es>
2024-07-05 12:38:34 +0200

    Add hash function to hashmap class

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -4,4 +4,16 @@ class HashMap
     @load_factor = load_factor
     @REAL_NUMBER = 0.5*(Math.sqrt(5)-1)
   end
+  # Check this out later
+  # https://www.cs.hmc.edu/~geoff/classes/hmc.cs070.200101/homework10/hashfuncs.html
+  
+  # only takes in string type keys
+  def hash(key)
+    hash_code = 0
+    prime_number = 89
+
+    key.each_char { |char| prime_number * hash_code + char.ord }
+    
+    hash_code
+  end
 end
