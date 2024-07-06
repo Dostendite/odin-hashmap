3e882238cccef0e0ce7598255c624c15e2ec96fc
Dostendite <dostendite@outlook.es>
2024-07-06 14:38:53 +0200

    Add attribute readers to Node class

--- lib/node.rb
+++ lib/node.rb
@@ -1,6 +1,13 @@
+# Node element containing a key & value pair for a hashmap
 class Node
-    def initialize(key, value)
-        @key = key
-        @value = value
-    end
+  attr_reader :key, :value
+
+  def initialize(key, value)
+    @key = key
+    @value = value
+  end
+
+  def to_s
+    "#{key} -> #{value}"
+  end
 end
